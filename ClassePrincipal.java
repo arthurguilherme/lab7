5c80a8ba29860e9ba965dccce515f6a7fdb77fa9
Guilherme <arthurguilherme@dcc.ufrj.br>
2021-02-01 23:24:07 -0300

    Update ClassePrincipal.java

--- ClassePrincipal.java
+++ ClassePrincipal.java
@@ -12,15 +12,20 @@ public class ClassePrincipal {
 	/**
 	 * @param args
 	 */
+	static int nP = 4, nC = 4; // pode variar aqui os valores do numero de threads Produtoras e consumidoras
+	
 	public static void main(String[] args) {
 
 		// inicializa o Buffer com o monitor
 		InsereERetiraElementos monitor = new InsereERetiraElementos();
 
 		// cria as threads produtoras e consumidoras
-		for (int i = 0; i < 4; i++) {
-			new Thread(new Produtor(monitor, i)).start();
-			new Thread(new Consumidor(monitor)).start();
+		for (int i = 0; i < nP; i++) {
+			new Thread(new Produtor(monitor)).start();
 		}
+		for (int i = 0; i < nC; i++) {
+			new Thread(new Produtor(monitor)).start();
+		}
+		
 	}
 }
