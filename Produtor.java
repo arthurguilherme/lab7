031393ace1422611873cdd7aa9c87be28f94e316
Guilherme <arthurguilherme@dcc.ufrj.br>
2021-02-01 23:18:01 -0300

    Update Produtor.java

--- Produtor.java
+++ Produtor.java
@@ -2,11 +2,10 @@ package lab7;
 
 import java.util.Random;
 
-public class Produtor implements Runnable{
-	
+public class Produtor implements Runnable {
+
 	InsereERetiraElementos monitor;
 	Random geraElemento = new Random();
-	
 
 	/**
 	 * @param monitor
@@ -17,9 +16,9 @@ public class Produtor implements Runnable{
 
 	@Override
 	public void run() {
-		while(true) {
+		while (true) {
 			try {
-				monitor.insereElemento(geraElemento.nextInt(8)+1);
+				monitor.insereElemento(geraElemento.nextInt(8) + 1);
 				monitor.imprimeBuffer();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
@@ -27,6 +26,5 @@ public class Produtor implements Runnable{
 			}
 		}
 	}
-	
 
 }
