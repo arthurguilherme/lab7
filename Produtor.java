c0d09cd9416337980c2ae5996c4136ad8396b1b5
Guilherme <arthurguilherme@dcc.ufrj.br>
2021-02-01 22:57:29 -0300

    Update Produtor.java

--- Produtor.java
+++ Produtor.java
@@ -1,5 +1,31 @@
 package lab7;
 
-public class Produtor {
+public class Produtor implements Runnable{
+	
+	InsereERetiraElementos monitor;
+	Integer elemento;
+	
+
+	/**
+	 * @param monitor
+	 * @param elemento
+	 */
+	public Produtor(InsereERetiraElementos monitor, Integer elemento) {
+		this.monitor = monitor;
+		this.elemento = elemento;
+	}
+
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				monitor.insereElemento(elemento);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
 
 }
