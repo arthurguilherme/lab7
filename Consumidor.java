e3aceee2bbcc1eafdabb112c0b85c064b0462fc0
Guilherme <arthurguilherme@dcc.ufrj.br>
2021-02-01 22:58:14 -0300

    Update Consumidor.java

--- Consumidor.java
+++ Consumidor.java
@@ -1,5 +1,27 @@
 package lab7;
 
-public class Consumidor {
+public class Consumidor implements Runnable {
+
+	InsereERetiraElementos monitor;
+
+	/**
+	 * @param monitor
+	 */
+	public Consumidor(InsereERetiraElementos monitor) {
+		this.monitor = monitor;
+	}
+
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				monitor.retiraElemento();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
 
 }
